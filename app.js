b5cee2e77d9a610e93c7c2bee699a9554d894c33
Gary Siu <Gary@untypeable.com>
2016-06-23 14:03:56 +0100

    Initial test build

---
+++ app.js
@@ -0,0 +1,49 @@
+tabris.ui.set("toolbarVisible", false);
+var MARGIN = 20;
+var logo;
+
+var page = new tabris.Page({
+  title: 'Coposition',
+  style: ["FULLSCREEN"],
+  topLevel: true
+}).once('resize', function () {
+  logo = new tabris.ImageView({
+    layoutData: {
+      top: MARGIN,
+      width: percentOfSmallest(25, 300),
+      centerX: 0
+    },
+    image: './images/logo.png',
+  }).appendTo(page);
+})
+
+var startButton = new tabris.Button({
+  centerY: -24,
+  left: MARGIN,
+  right: MARGIN,
+  background: '#FF7E00',
+  textColor: '#FFF',
+  font: '24px',
+  text: 'START'
+}).appendTo(page);
+
+var textView = new tabris.TextView({
+  centerX: 0, top: [startButton, 50],
+  font: '24px'
+}).appendTo(page);
+
+function percentOfSmallest (percentage, imageSize) {
+  var bounds = page.get('bounds');
+  var smallest = bounds.height > bounds.width ? bounds.width : bounds.height;
+  var computed = smallest / 100 * percentage;
+  if(computed < imageSize) {
+    return ~~computed
+  }
+  return imageSize;
+}
+
+startButton.on('select', function() {
+  textView.set('text', percentOfSmallest(25, 200));
+});
+
+page.open();
